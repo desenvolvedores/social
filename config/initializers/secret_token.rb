b10b0447d17d653e922da7aaaaca6f8bcd88f750
Marcio Luis Rodrigues de Paula Lima <marcio@hexadc.com.br>
2013-06-24 13:55:04 -0300

    commit inicial
    primeira carga dos arquivos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Social::Application.config.secret_token = 'd9c1fc64834f10b551dfc6b90928c17a12778a2916a02f2b835b8023294fc6de91cf5a4c0bad8c937c51fd068521d8d00dfad4d6d540a2a586297c0343265490'
